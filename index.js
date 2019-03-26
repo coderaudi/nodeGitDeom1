136d5edc0f9cf3bb19619295435e809d38b99070
Abhijeet Khire <abhijeet.khire@Eternus.local>
2019-03-26 14:05:13 +0530

    node commit without node_module folder .gitignore is used rootfolder to mention the names of unwanted folders

---
+++ index.js
@@ -0,0 +1,11 @@
+var _ = require('underscore');
+
+// 1 it look underscore is core module
+// 2. underscore is file/folder
+// 3. assume it is in node_module
+
+// use the utility of the underscore library 
+
+var reseult = _.contains([1,2,3,4,5] , 3);
+
+console.log(reseult);
